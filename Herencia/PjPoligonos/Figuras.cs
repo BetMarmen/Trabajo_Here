33caee6557d8d21a6ffe93719a789c172a87a0a5
Francisco <Ruiz31348@gmail.com>
2023-09-11 21:55:16 -0600

    Se agregaron los atributos

--- Herencia/PjPoligonos/Figuras.cs
+++ Herencia/PjPoligonos/Figuras.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PjPoligonos
 {
-    internal class Figuras
+    public class Figuras
     {
+        public double DiametroCir { set; get; }
+        public double DiametroEsf { set; get; }
+        public double BaseTri { set; get; }
+        public double AlturaTri { set; get; }
+        public double BaseCuad { set; get; }
+        public double AlturaCuad { set; get; }
+
+        public double BaseCubo { set; get; }
+        public double AlturaCubo { set; get; }
+        public double BaseTetra { set; get; }
+        public double AlturaTetra { set; get; }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
